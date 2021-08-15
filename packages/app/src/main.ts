76180eedd4013ed7c1b473cbf706182ff23a2b0a
retn0 <nbsp1221@gmail.com>
2021-08-15 20:26:44 +0900

    👔 [app] Add logic about 'paybooc' attendance check event

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -1,11 +1,48 @@
+import dotenv from 'dotenv';
 import { HttpClient } from '@fortune-assistant/utils-http-client';
 import { scheduler } from '@fortune-assistant/utils-scheduler';
 
-const httpClient = new HttpClient();
+dotenv.config();
 
-scheduler.schedule('*/5 * * * * *', async () => {
-  const response = await httpClient.get('https://www.paybooc.co.kr/app/paybooc/Main.do');
+const PAYBOOC_SESSION_ID = process.env.PAYBOOC_SESSION_ID ?? '';
 
-  console.log('every 5 seconds');
-  console.log(response.data);
-});
+function consoleLog(log: unknown) {
+  console.log(`[${new Date().toLocaleString('en-US', { timeZone: 'Asia/Seoul' })}]`, log);
+}
+
+function startPayboocAttendanceCheck() {
+  consoleLog('페이북 시작');
+
+  const httpClient = new HttpClient({
+    headers: {
+      contentType: 'application/x-www-form-urlencoded',
+      cookie: `JSESSIONID=${PAYBOOC_SESSION_ID}`,
+    },
+  });
+
+  const attendanceCheck = async () => {
+    const response = await httpClient.post<string, { resData: { returnMessage: string } }>(
+      'https://paybooc.co.kr/app/paybooc/RLoginDailyEventJson.do',
+      'retKey=json&encEvntNo=186tCXAUmCfxcrAKd5PVew%3D%3D&opType=apply'
+    );
+
+    consoleLog(`출석 체크 시도 - ${response.data.resData.returnMessage}`);
+  };
+
+  attendanceCheck();
+
+  scheduler.schedule('0 0 9 * * *', async () => {
+    attendanceCheck();
+  }, {
+    timezone: 'Asia/Seoul',
+  });
+
+  scheduler.schedule('*/5 * * * *', async () => {
+    const response = await httpClient.get<string>('https://paybooc.co.kr/app/paybooc/Main.do');
+    consoleLog(`세션 유지 시도 - ${response.statusCode}`);
+  }, {
+    timezone: 'Asia/Seoul',
+  });
+}
+
+startPayboocAttendanceCheck();
